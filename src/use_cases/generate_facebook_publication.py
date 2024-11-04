69e60c4a67fd5d982933b184b990d1818e14cc16
tech_aware <techaware.ai@gmail.com>
2024-11-04 17:47:49 +0100

    feat(generate_facebook_publication): Adding instruction to prompt message

--- src/use_cases/generate_facebook_publication.py
+++ src/use_cases/generate_facebook_publication.py
@@ -44,7 +44,6 @@ class GenerateFacebookPublicationUseCase:
                 "conversational, and suitable for a general audience. "
                 "Include relevant emojis where appropriate. "
                 "Include link without any special format, writing it as https://www.webpage.net"
-
             )
             generated_publication = self.openai_gateway.generate(facebook_prompt)
             logger.debug(f"Facebook publication generated: {generated_publication}")
